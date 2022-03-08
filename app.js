d191887cef94c3a19a20e4a89305e4a16d87c7ea
omsoo <onyebuchisundayo@gmail.com>
2022-03-08 19:46:41 +0100

    Check Linters error for index.html, style.css and the app.js files

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ function show() {
   mainMenu.style.display = 'flex';
   mainMenu.style.top = '0';
 }
+
 function close() {
   mainMenu.style.top = '-100%';
 }
+
 slideMenu.forEach((slideMenuCheck) => {
   slideMenuCheck.addEventListener('click', close);
 });
 
 openMenu.addEventListener('click', show);
+
 closeMenu.addEventListener('click', close);
