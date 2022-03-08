1401b677c4e4c6106c98f1d4fdab9451774d38ea
omsoo <onyebuchisundayo@gmail.com>
2022-03-08 19:21:24 +0100

    Update the app.js file

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const mainMenu = document.querySelector(".mainMenu");
-const closeMenu = document.querySelector(".closeMenu");
-const openMenu = document.querySelector(".openMenu");
-const slideMenu = document.querySelectorAll(".slideMenu");
-
-openMenu.addEventListener("click", show);
-closeMenu.addEventListener("click", close);
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const slideMenu = document.querySelectorAll('.slideMenu');
 
 function show() {
-  mainMenu.style.display = "flex";
-  mainMenu.style.top = "0";
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
 }
 function close() {
-  mainMenu.style.top = "-100%";
+  mainMenu.style.top = '-100%';
 }
-slideMenu.forEach((slide_Menu) => {
-  slide_Menu.addEventListener("click", close);
+slideMenu.forEach((slideMenuCheck) => {
+  slideMenuCheck.addEventListener('click', close);
 });
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
