a11a74bdc9eb6244bd37f1600eac5f91c96aa3df
omsoo <onyebuchisundayo@gmail.com>
2022-03-08 13:36:09 +0100

    Add navbar close button

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 // setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
+// const navBtn = document.getElementById("nav-btn");
+// const navbar = document.getElementById("navbar");
+// const navClose = document.getElementById("nav-close");
 // show nav
-navBtn.addEventListener("click", () => {
-  console.log("Clicked");
-  navbar.classList.add("showNav");
-});
+// navBtn.addEventListener("click", () => {
+//   console.log("Clicked");
+//   navbar.classList.add("showNav");
+// });
 // close nav
-navClose.addEventListener("click", () => {
-  console.log("Clicked");
-  navbar.classList.remove("showNav");
-});
+// navClose.addEventListener("click", () => {
+//   console.log("Clicked");
+//   navbar.classList.remove("showNav");
+// });
+
+const mainMenu = document.querySelector(".mainMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+function close() {
+  mainMenu.style.top = "-100%";
+}
